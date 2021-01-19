65f94c92decca3d13c9d58afa4935da68c6a48bf
unisolzp <unisolzp@gmail.com>
2021-01-19 16:28:19 +0200

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,17 +1,2 @@
 #!/bin/sh
-
-set -x
-set -e
-
-if [ -z "${USERNAME}" ]; then
-  echo "You have to specify the -e USERNAME=... argument"
-  exit
-fi
-if [ -z "${PASSWORD}" ]; then
-  echo "You have to specify the -e PASSWORD=... argument"
-  exit
-fi
-
-htpasswd -cb /etc/squid/passwd "${USERNAME}" "${PASSWORD}"
-
 ( socat tcp-listen:$PORT,reuseaddr,fork tcp:localhost:3129 ) & exec $(which squid) -NYCd 1
